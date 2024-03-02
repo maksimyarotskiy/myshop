e6599274861e125fb75ee1617364de55dac02c87
Maksim Yarotskiy <iartoskiimaksim@gmail.com>
2024-03-02 22:23:54 +0300

    - сделал редирект после создания пользователя - поправил поля на форме - поправил searchbar на главной

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 from django.views import View
 from django.shortcuts import render, redirect
 from django import forms
@@ -16,9 +17,13 @@ class RegisterUser(CreateView):
     form_class = SignUpForm
     template_name = 'registration/signup.html'
     extra_context = {'title': "Registration"}
+    success_url = reverse_lazy('home')
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
 
-
+        return response
 
 # class SignUp(View):
 #     template_name = 'registration/signup.html'
