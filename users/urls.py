5e0c5645fdb9c059111bbd9e70a2eb036be60901
Maksim Yarotskiy <iartoskiimaksim@gmail.com>
2024-03-10 01:01:13 +0300

    -Добавил футер -Верстка с изменением пароля -Добавил пути для password_change !пароль через форму меняется , но есть ошибка

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 from django.urls import path, include
 
 from users.views import *
@@ -10,4 +10,9 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('signup/', RegisterUser.as_view(), name='signup'),
     path('profile/', ProfileUser.as_view(), name='profile'),
+    path(
+        'password-change/done/', PasswordChangeView.as_view(template_name='password_change_form.html'),
+         name='password_change'
+         ),
+    path('password-change/', PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
